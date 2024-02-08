cba24a7333a15937682eb8dbbebb13b8f712948a
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-08 12:35:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+
+import './App.css';
+import Map from './components/map';
+
+function App() {
+  return (
+    <>
+      
+      <h1>Hi NATALIA!</h1>
+      <Map latitude={47.4211} longitude={10.9854} />
+    </> 
+  );
+}
+
+export default App;
---
+++ src/components/map.jsx
@@ -0,0 +1,23 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import "leaflet/dist/leaflet.css"; // Ensure this import is present to avoid map display issues
+
+function Map( {latitude, longitude }) {
+  const position = [latitude, longitude]
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "400px" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
