fdeda0ab00fe85b60045d591d9bb3b62faa361d2
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-15 16:30:09 +0100

    2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ function HomePage() {
 
       <div className="homePage">
         <img
-          src="../public/images/mountain.jpg"
+          src="../public/mountain.jpg"
           alt="big-mountain-pic"
           className="big-mountain-pic"
         />
