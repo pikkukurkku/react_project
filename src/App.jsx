c3a414d2cbea327f10f7b9b46fa771cc96e73995
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-15 15:09:15 +0100

    added hash router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar";
 import HomePage from "./pages/HomePage";
 import AllHikes from "./pages/AllHikes";
 import HikeDeets from "./pages/HikeDeets";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import NewReview from "./pages/NewReview";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -13,15 +13,18 @@ import EditReviewForm from "./components/EditReviewForm";
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/hikes" element={<AllHikes />} />
         <Route path="/hikes/:hikeId" element={<HikeDeets />} />
         <Route path="/hikes/:hikeId/new-review" element={<NewReview />} />
-        <Route path="/hikes/:hikeId/reviews/:reviewId/edit" element={<EditReviewForm />} />
+        <Route
+          path="/hikes/:hikeId/reviews/:reviewId/edit"
+          element={<EditReviewForm />}
+        />
       </Routes>
-    <Footer />
+      <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
