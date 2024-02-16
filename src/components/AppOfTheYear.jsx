50892ee1842010e021ebdb753eb38cefa2f741fc
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-16 16:00:40 +0100

    update pics urls

--- src/components/AppOfTheYear.jsx
+++ src/components/AppOfTheYear.jsx
@@ -1,10 +1,11 @@
 import './AppOfTheYear.css'
 
 function AppOfTheYear () {
+    const logo = "./HikeLove.webp"
     return (
         <div className="AppOfTheYear">
             <h1>App of the Year!</h1>
-            <img src="./HikeLove.webp" alt="hiking-logo" className="hiking-logo"/>
+            <img src={logo} alt="hiking-logo" className="hiking-logo"/>
             <h2>Hikelove is the award winning hiking app</h2>
             <h3>Download here</h3>
             <img src="./appStoreIos.gif" alt="App-store-download-icon" />
