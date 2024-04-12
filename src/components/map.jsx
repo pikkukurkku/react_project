7da17c88c22db901ddad733d089a41300445e863
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-04-12 16:15:19 +0200

    update markers for map

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,5 +1,14 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; 
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css"; 
+import L from 'leaflet'; // Import the Leaflet library
+
+// Define the path to the marker and shadow icon images
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
+  iconUrl: require("leaflet/dist/images/marker-icon.png").default,
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+});
 
 function Map( {latitude, longitude }) {
   const position = [latitude, longitude]
