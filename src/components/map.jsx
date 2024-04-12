8ddef2b4901c91cc3c2c78815325e12e02958eb0
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-04-12 16:18:58 +0200

    updated markers

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,28 +1,27 @@
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "leaflet/dist/leaflet.css"; 
-import L from 'leaflet'; // Import the Leaflet library
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; 
 
-// Define the path to the marker and shadow icon images
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
-  iconUrl: require("leaflet/dist/images/marker-icon.png").default,
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-});
+function Map({ latitude, longitude }) {
+  const position = [latitude, longitude];
 
-function Map( {latitude, longitude }) {
-  const position = [latitude, longitude]
+  let defaultIcon = new L.Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+  });
 
   return (
-    
     <MapContainer center={position} zoom={9} style={{ height: "100%", width: "100%" }}>
-   
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      
-      <Marker position={position}>
+      <Marker position={position} icon={defaultIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
@@ -32,4 +31,3 @@ function Map( {latitude, longitude }) {
 }
 
 export default Map;
-
